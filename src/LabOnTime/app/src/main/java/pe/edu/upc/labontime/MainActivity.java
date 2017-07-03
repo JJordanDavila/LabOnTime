8497604da148db15d577f1bb1e6d97c185d5c184
George Quispitongo <g3org3.18.94@gmail.com>
2017-07-02 22:32:53 -0500

    Login y frame de pacientes

--- src/LabOnTime/app/src/main/java/pe/edu/upc/labontime/MainActivity.java
+++ src/LabOnTime/app/src/main/java/pe/edu/upc/labontime/MainActivity.java
@@ -1,45 +1,65 @@
 package pe.edu.upc.labontime;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    Button loginButton;
+    EditText usuarioEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /* Intent intent = new Intent(MainActivity.this,PacienteActivity.class);
+                startActivity(intent);*/
+               login();
+            }
+
+        });
+
     }
+
+    public void login(){
+
+        usuarioEditText = (EditText) findViewById(R.id.usuarioEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        if(usuarioEditText.getText().toString().equals("admin") &&
+                passwordEditText.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(),
+                    "Redirecting...",Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this,PacienteActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "Wrong Credentials...",Toast.LENGTH_SHORT).show();
+
+            usuarioEditText.setVisibility(View.VISIBLE);
+            usuarioEditText.setBackgroundColor(Color.RED);
+
+        }
+    }
+
+
 
 }

c5d50a691ba1b5e3615a428ccc2291727af926bf
wcarranzab <waltericardo10@gmail.com>
2017-07-15 21:54:16 -0500

    wcarranza-demo

--- src/LabOnTime/app/src/main/java/pe/edu/upc/labontime/activities/MainActivity.java
+++ src/LabOnTime/app/src/main/java/pe/edu/upc/labontime/activities/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText usuarioEditText;
     EditText passwordEditText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
